c808359c4e92e46b7d7192721d526f2c4d6e81a9
JustAnOtherCoder13 <justanothercoder13@gmail.com>
2020-03-04 15:58:12 +0100

    everything work well, now go on detail! be careful that delete button now delete in  the two list...

--- Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourActivityDetail.java
+++ Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourActivityDetail.java
@@ -3,8 +3,6 @@ package com.openclassrooms.entrevoisins.ui.neighbour_list;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
